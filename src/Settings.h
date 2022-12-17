4e2259054e4d538bf13b35c120a8784483dbb7d0
Narase33 <57033854+Narase33@users.noreply.github.com>
2022-12-17 09:04:31 +0100

    Update Settings.h

---
+++ src/Settings.h
@@ -0,0 +1,6 @@
+#pragma once
+
+static constexpr const char* bot_name = "";
+static constexpr const char* bot_password = "";
+static constexpr const char* bot_id = "";
+static constexpr const char* bot_secret = "";
